4e0130ad57a43c7da8da953eaa23f48dc4f66ea2
rahul <navnathdevikar1234@gmail.com>
2022-03-09 13:21:01 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/APITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.junit.Test;
+
+public class APITest {
+
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.junit.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	@Test
+	public void iosApp()
+	{
+		System.out.println("iosApp");
+	}
+}
